e1a528e4263a009f8d6b5ab1287ca43881ac8338
Your Name <you@example.com>
2025-05-18 15:09:10 +0700

    cсоздал константу для товаров

---
+++ src/const/modal-type.ts
@@ -0,0 +1,6 @@
+export enum ModalType {
+  AddCameraInBasket = 'Добавление товара',
+  CameraAddedToBasket = 'Товар добавлен в корзину',
+  RemoveCameraFromBasket = 'Удаление товара из корзины',
+  CamerasOrdered = 'Заказ отправлен на сервер'
+}
