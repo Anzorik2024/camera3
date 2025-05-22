9e8083e7e36f7640c4bcd70ef3dcbb5fcc0f56f2
Your Name <you@example.com>
2025-05-22 17:11:14 +0700

    блокировка кнопки при пустой корзине

--- src/components/basket-order/basket-order.tsx
+++ src/components/basket-order/basket-order.tsx
@@ -1,21 +1,38 @@
+import { formatPrice } from '../../utils/format';
+
 type BasketOrderProps = {
  // discountPrice: number;
   totalPrice: number;
- // isBasketEmpty: boolean;
+  isBasketEmpty: boolean;
   onOrderButtonClick: () => void;
 }
 
-function BasketOrder({ totalPrice, onOrderButtonClick}: BasketOrderProps): JSX.Element {
+function BasketOrder({ totalPrice, isBasketEmpty, onOrderButtonClick}: BasketOrderProps): JSX.Element {
 
   const handleOrderButtonClick = () => {
     onOrderButtonClick();
   };
   return(
     <div className="basket__summary-order">
-      <p className="basket__summary-item"><span className="basket__summary-text">Всего:</span><span className="basket__summary-value">111 390 ₽</span></p>
-      <p className="basket__summary-item"><span className="basket__summary-text">Скидка:</span><span className="basket__summary-value basket__summary-value--bonus">0 ₽</span></p>
-      <p className="basket__summary-item"><span className="basket__summary-text basket__summary-text--total">К оплате:</span><span className="basket__summary-value basket__summary-value--total">111 390 ₽</span></p>
-      <button className="btn btn--purple" type="submit" onClick={handleOrderButtonClick}>Оформить заказ
+      <p className="basket__summary-item">
+        <span className="basket__summary-text">Всего:</span>
+        <span className="basket__summary-value">{formatPrice(totalPrice)} ₽</span>
+      </p>
+      <p className="basket__summary-item">
+        <span className="basket__summary-text">Скидка:</span>
+        <span className="basket__summary-value basket__summary-value--bonus">0 ₽</span>
+      </p>
+      <p className="basket__summary-item">
+        <span className="basket__summary-text basket__summary-text--total">К оплате:</span>
+        <span className="basket__summary-value basket__summary-value--total">111 390 ₽</span>
+      </p>
+      <button
+        className="btn btn--purple"
+        type="submit"
+        disabled={isBasketEmpty}
+        onClick={handleOrderButtonClick}
+      >
+          Оформить заказ
       </button>
     </div>
   );
--- src/components/basket-summary/basket-summary.tsx
+++ src/components/basket-summary/basket-summary.tsx
@@ -1,11 +1,24 @@
-import BasketOrder from "../basket-order/basket-order";
+import BasketOrder from '../basket-order/basket-order';
+import { useAppDispatch } from '../../hooks/use-app-dispatch';
+import { useAppSelector } from '../../hooks/use-app-selector';
+import { getCamerasInTheBasket } from '../../store/selectors';
+
+import { Camera } from '../../types/camera';
+
+
 type BasketSummaryProps = {
   onModalInfoOpen: () => void;
 };
 function BasketSummary ({ onModalInfoOpen }: BasketSummaryProps): JSX.Element {
 
-  const handleOrderButtonClick = () => {
+  const dispatch = useAppDispatch();
 
+  const camerasInBasket = useAppSelector(getCamerasInTheBasket);
+  const isBasketEmpty = camerasInBasket.length === 0;
+  const camerasInBasketTotalPrice = camerasInBasket.reduce((acc: number, item: Camera) => acc + item.price, 0);
+
+
+  const handleOrderButtonClick = () => {
     onModalInfoOpen();
   };
   return(
@@ -14,6 +27,7 @@ function BasketSummary ({ onModalInfoOpen }: BasketSummaryProps): JSX.Element {
       </div>
       <BasketOrder
         totalPrice={camerasInBasketTotalPrice}
+        isBasketEmpty={isBasketEmpty}
         onOrderButtonClick={handleOrderButtonClick}
       />
     </div>
