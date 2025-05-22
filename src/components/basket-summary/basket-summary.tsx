733643a71091c624a2b51ab871a4f70ad996c16f
Your Name <you@example.com>
2025-05-22 17:03:28 +0700

    вынос заказа из корзины

--- src/components/basket-summary/basket-summary.tsx
+++ src/components/basket-summary/basket-summary.tsx
@@ -1,4 +1,4 @@
-
+import BasketOrder from "../basket-order/basket-order";
 type BasketSummaryProps = {
   onModalInfoOpen: () => void;
 };
@@ -12,13 +12,10 @@ function BasketSummary ({ onModalInfoOpen }: BasketSummaryProps): JSX.Element {
     <div className="basket__summary">
       <div className="basket__promo">
       </div>
-      <div className="basket__summary-order">
-        <p className="basket__summary-item"><span className="basket__summary-text">Всего:</span><span className="basket__summary-value">111 390 ₽</span></p>
-        <p className="basket__summary-item"><span className="basket__summary-text">Скидка:</span><span className="basket__summary-value basket__summary-value--bonus">0 ₽</span></p>
-        <p className="basket__summary-item"><span className="basket__summary-text basket__summary-text--total">К оплате:</span><span className="basket__summary-value basket__summary-value--total">111 390 ₽</span></p>
-        <button className="btn btn--purple" type="submit" onClick={handleOrderButtonClick}>Оформить заказ
-        </button>
-      </div>
+      <BasketOrder
+        totalPrice={camerasInBasketTotalPrice}
+        onOrderButtonClick={handleOrderButtonClick}
+      />
     </div>
   );
 }
