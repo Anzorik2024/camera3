5127104cefa3abb7436e78f55b79a3f26d92873a
Your Name <you@example.com>
2025-05-22 13:45:54 +0700

    fix bug basket

--- src/pages/basket-page/basket-page.tsx
+++ src/pages/basket-page/basket-page.tsx
@@ -14,6 +14,7 @@ import { WarningMessage } from '../../const/warning-message';
 import { useAppDispatch } from '../../hooks/use-app-dispatch';
 import { selectCamera } from '../../store/order-slice/order-slice';
 
+
 function BasketPage() : JSX.Element {
 
   const cameras = useAppSelector(getCamerasInTheBasket);
