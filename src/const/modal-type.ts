6fa079f97f93e971d33ed5f5f5c13d11334e35ae
Your Name <you@example.com>
2025-06-01 21:07:17 +0700

    чистка конастант и тестов

--- src/const/modal-type.ts
+++ src/const/modal-type.ts
@@ -2,6 +2,5 @@ export enum ModalType {
   AddCameraInBasket = 'Добавить товар в корзину',
   CameraAddedToBasket = 'Товар  успешно добавлен в корзину',
   RemoveCameraFromBasket = 'Удалить  этот товар?',
-  CamerasOrdered = 'Спасибо за покупку',
-  ReviewGratitude = 'Спасибо за отзыв',
+  CamerasOrdered = 'Спасибо за покупку'
 }
